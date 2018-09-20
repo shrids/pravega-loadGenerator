c8d4462722900ad9a374ec62f03fa99670f156ce
Sandeep <sandeep.shridhar@emc.com>
2018-09-20 05:42:03 -0400

    Initial version.
    Signed-off-by: Sandeep <sandeep.shridhar@emc.com>

---
+++ src/main/java/io/pravega/loadTester/Main.java
@@ -0,0 +1,15 @@
+package io.pravega.loadTester;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * Main class
+ */
+@Slf4j
+public class Main {
+
+    public static void main(String[] args) {
+        log.info("Hello");
+        System.out.println("hello world");
+    }
+}
